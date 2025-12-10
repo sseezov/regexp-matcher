54f07ec1979dbb4ab35bd4ed8b5856721a1c094c
sseezov <sseezov@mail.ru>
2025-12-10 15:39:47 +0700

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const regexpForm = document.querySelector('.regexpForm')
+const regexpInput = document.querySelector('#regexpInput')
+const textInput = document.querySelector('#textInput')
+const result = document.querySelector('.result')
+
+regexpForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const regexp = new RegExp(regexpInput.value, 'g');
+  const input = textInput.value
+  const match = input.match(regexp);
+  const newInputElements = input.split('').map((symbol) => {
+    if (match.includes(symbol)){
+      return `<p class="match">${symbol}</p>`;
+    }
+    return `<p>${symbol}</p>`;
+  }).join('')
+
+  result.innerHTML = newInputElements;
+})
